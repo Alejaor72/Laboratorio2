5eb23418c72d8e874982bda2f455bd3760122b85
Your Name <you@example.com>
2023-03-09 22:15:52 -0500

    data update

---
+++ public/src/components/data3.js
@@ -0,0 +1,39 @@
+const data3 =  [
+    {
+        product: 'Dew Tint - 14 Colors',
+        brand: 'romand',
+        price: 'US$ 16.00',
+        deal: 'Deal is 88% claimed',
+        image: 'https://d1flfk77wl2xk4.cloudfront.net/Assets/01/429/M_p0166142901.jpg'
+    },
+    {
+        product: 'Matte Sun Stick',
+        brand: 'romand',
+        price: 'US$ 14.40',
+        deal: 'Deal is 88% claimed',
+        image: 'https://d1flfk77wl2xk4.cloudfront.net/Assets/13/138/M_p0188313813.jpg'
+    },
+    {
+        product: 'Glow Deep Serum',
+        brand: 'romand',
+        price: 'US$ 12.75',
+        deal: 'Deal is 88% claimed',
+        image: 'https://d1flfk77wl2xk4.cloudfront.net/Assets/32/268/M_p0153126832.jpg'
+    },
+    {
+        product: 'Acne Pimple Master Patch',
+        brand: 'romand',
+        price: 'US$ 3.08',
+        deal: 'Deal is 88% claimed',
+        image: 'https://d1flfk77wl2xk4.cloudfront.net/Assets/07/812/M_p0056481207.jpg'
+    },
+    {
+        product: 'NEW Last Auto Gel Eyeliner - 10 Colors',
+        brand: 'romand',
+        price: 'US$ 5.32',
+        deal: 'Deal is 88% claimed',
+        image: 'https://d1flfk77wl2xk4.cloudfront.net/Assets/59/810/M_p0108781059.jpg'
+    }
+]
+
+export default data3;
